c157d500d169303efe5796363d6a90ae6ea33fe7
Vladimir Shabanov <vshabanov@griddynamics.com>
2023-01-18 21:40:24 +0100

    Read headers

--- src/main/java/com/example/candlesubstractor/Starter.java
+++ src/main/java/com/example/candlesubstractor/Starter.java
@@ -5,16 +5,28 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.io.FileReader;
-import java.util.Arrays;
-import java.util.List;
 
 @Component
 public class Starter implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         try (CSVReader reader = new CSVReader(new FileReader(args[0]))) {
-            List<String[]> rows = reader.readAll();
-            rows.forEach(row -> System.out.println(Arrays.toString(row)));
+            String[] headers = reader.readNext();
+            String h = getColumnIndex(headers);
+            System.out.println(h);
         }
+    }
+
+    private String getColumnIndex(String[] headers) {
+        int index = 0;
+        String h = null;
+        for (String header : headers) {
+            if (header.equals("<DATE>")) {
+                System.out.println("index = " + index);
+                h = header;
+            }
+            index++;
+        }
+        return h;
     }
 }
