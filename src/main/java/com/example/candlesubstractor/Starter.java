f308ab0a339eba8af7b1f4a27a4dc8a014a9fd2e
Vladimir Shabanov <vshabanov@griddynamics.com>
2023-01-18 21:28:52 +0100

    Read first file

--- src/main/java/com/example/candlesubstractor/Starter.java
+++ src/main/java/com/example/candlesubstractor/Starter.java
@@ -1,12 +1,20 @@
 package com.example.candlesubstractor;
 
+import com.opencsv.CSVReader;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class Starter implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        try (CSVReader reader = new CSVReader(new FileReader(args[0]))) {
+            List<String[]> rows = reader.readAll();
+            rows.forEach(row -> System.out.println(Arrays.toString(row)));
+        }
     }
 }
